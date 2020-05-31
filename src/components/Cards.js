c2b1ab9c96d320335ece3f7acc28e938d6bc9848
Ann Hoang <lph2yr@virginia.edu>
2020-05-30 23:06:46 -0400

    add more cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -52,162 +52,162 @@ export var DECK = [
         suit: "Spades"
     },
 
-    //{
-    //     num: 14,
-    //     suit: "Hearts"
-    // },
-    // {
-    //     num: 2,
-    //     suit: "Hearts"
-    // },
-    // {
-    //     num: 3,
-    //     suit: "Hearts"
-    // },
-    // {
-    //     num: 4,
-    //     suit: "Hearts"
-    // },
-    // {
-    //     num: 5,
-    //     suit: "Hearts"
-    // },
-    // {
-    //     num: 6,
-    //     suit: "Hearts"
-    // },
-    // {
-    //     num: 7,
-    //     suit: "Hearts"
-    // },
-    // {
-    //     num: 8,
-    //     suit: "Hearts"
-    // },
-    // {
-    //     num: 9,
-    //     suit: "Hearts"
-    // },
-    // {
-    //     num: 10,
-    //     suit: "Hearts"
-    // },
-    // {
-    //     num: 11,
-    //     suit: "Hearts"
-    // },
-    // {
-    //     num: 12,
-    //     suit: "Hearts"
-    // },
-    // {
-    //     num: 13,
-    //     suit: "Hearts"
-    // },
+    {
+        num: 14,
+        suit: "Hearts"
+    },
+    {
+        num: 2,
+        suit: "Hearts"
+    },
+    {
+        num: 3,
+        suit: "Hearts"
+    },
+    {
+        num: 4,
+        suit: "Hearts"
+    },
+    {
+        num: 5,
+        suit: "Hearts"
+    },
+    {
+        num: 6,
+        suit: "Hearts"
+    },
+    {
+        num: 7,
+        suit: "Hearts"
+    },
+    {
+        num: 8,
+        suit: "Hearts"
+    },
+    {
+        num: 9,
+        suit: "Hearts"
+    },
+    {
+        num: 10,
+        suit: "Hearts"
+    },
+    {
+        num: 11,
+        suit: "Hearts"
+    },
+    {
+        num: 12,
+        suit: "Hearts"
+    },
+    {
+        num: 13,
+        suit: "Hearts"
+    },
 
-    // {
-    //     num: 14,
-    //     suit: "Diamonds"
-    // },
-    // {
-    //     num: 2,
-    //     suit: "Diamonds"
-    // },
-    // {
-    //     num: 3,
-    //     suit: "Diamonds"
-    // },
-    // {
-    //     num: 4,
-    //     suit: "Diamonds"
-    // },
-    // {
-    //     num: 5,
-    //     suit: "Diamonds"
-    // },
-    // {
-    //     num: 6,
-    //     suit: "Diamonds"
-    // },
-    // {
-    //     num: 7,
-    //     suit: "Diamonds"
-    // },
-    // {
-    //     num: 8,
-    //     suit: "Diamonds"
-    // },
-    // {
-    //     num: 9,
-    //     suit: "Diamonds"
-    // },
-    // {
-    //     num: 10,
-    //     suit: "Diamonds"
-    // },
-    // {
-    //     num: 11,
-    //     suit: "Diamonds"
-    // },
-    // {
-    //     num: 12,
-    //     suit: "Diamonds"
-    // },
-    // {
-    //     num: 13,
-    //     suit: "Diamonds"
-    // },
+    {
+        num: 14,
+        suit: "Diamonds"
+    },
+    {
+        num: 2,
+        suit: "Diamonds"
+    },
+    {
+        num: 3,
+        suit: "Diamonds"
+    },
+    {
+        num: 4,
+        suit: "Diamonds"
+    },
+    {
+        num: 5,
+        suit: "Diamonds"
+    },
+    {
+        num: 6,
+        suit: "Diamonds"
+    },
+    {
+        num: 7,
+        suit: "Diamonds"
+    },
+    {
+        num: 8,
+        suit: "Diamonds"
+    },
+    {
+        num: 9,
+        suit: "Diamonds"
+    },
+    {
+        num: 10,
+        suit: "Diamonds"
+    },
+    {
+        num: 11,
+        suit: "Diamonds"
+    },
+    {
+        num: 12,
+        suit: "Diamonds"
+    },
+    {
+        num: 13,
+        suit: "Diamonds"
+    },
 
-    // {
-    //     num: 14,
-    //     suit: "Clubs"
-    // },
-    // {
-    //     num: 2,
-    //     suit: "Clubs"
-    // },
-    // {
-    //     num: 3,
-    //     suit: "Clubs"
-    // },
-    // {
-    //     num: 4,
-    //     suit: "Clubs"
-    // },
-    // {
-    //     num: 5,
-    //     suit: "Clubs"
-    // },
-    // {
-    //     num: 6,
-    //     suit: "Clubs"
-    // },
-    // {
-    //     num: 7,
-    //     suit: "Clubs"
-    // },
-    // {
-    //     num: 8,
-    //     suit: "Clubs"
-    // },
-    // {
-    //     num: 9,
-    //     suit: "Clubs"
-    // },
-    // {
-    //     num: 10,
-    //     suit: "Clubs"
-    // },
-    // {
-    //     num: 11,
-    //     suit: "Clubs"
-    // },
-    // {
-    //     num: 12,
-    //     suit: "Clubs"
-    // },
-    // {
-    //     num: 13,
-    //     suit: "Clubs"
-    // }
+    {
+        num: 14,
+        suit: "Clubs"
+    },
+    {
+        num: 2,
+        suit: "Clubs"
+    },
+    {
+        num: 3,
+        suit: "Clubs"
+    },
+    {
+        num: 4,
+        suit: "Clubs"
+    },
+    {
+        num: 5,
+        suit: "Clubs"
+    },
+    {
+        num: 6,
+        suit: "Clubs"
+    },
+    {
+        num: 7,
+        suit: "Clubs"
+    },
+    {
+        num: 8,
+        suit: "Clubs"
+    },
+    {
+        num: 9,
+        suit: "Clubs"
+    },
+    {
+        num: 10,
+        suit: "Clubs"
+    },
+    {
+        num: 11,
+        suit: "Clubs"
+    },
+    {
+        num: 12,
+        suit: "Clubs"
+    },
+    {
+        num: 13,
+        suit: "Clubs"
+    }
 ]
